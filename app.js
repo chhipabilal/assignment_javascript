1873c866d97c511ffce33d971edc1065c3878087
chhipabilal <chhipabilal65@gmail.com>
2024-03-02 14:54:07 +0500

    assignment_javascript is created

---
+++ app.js
@@ -0,0 +1,90 @@
+
+var engish =+prompt('enter your marks')
+var urdu =+prompt('enter your marks')
+var math =+prompt('enter your marks')
+var accounting =+prompt('enter your marks')
+
+
+if(engish > 33){
+    console.log('you are pass')
+     }else{
+          console.log('you are fali')
+     }
+
+
+     if(urdu > 40){
+        console.log('you are pass')
+         }else{
+              console.log('you are fali')
+         }
+
+
+         if(math > 50){
+            console.log('you are pass')
+             }else{
+                  console.log('you are fali')
+             }
+
+             
+             if(accounting > 60){
+                console.log(' you are pass')
+                 }else{
+                      console.log('you are fali')
+                 }
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
